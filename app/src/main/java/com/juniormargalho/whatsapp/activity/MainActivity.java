46004b76df1651651ebc249624d5238610df5d39
juniormargalho <juniormargalho@gmail.com>
2019-11-16 11:40:25 -0300

    criacaoAbas

--- app/src/main/java/com/juniormargalho/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/juniormargalho/whatsapp/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.juniormargalho.whatsapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,11 @@ import android.view.MenuItem;
 import com.google.firebase.auth.FirebaseAuth;
 import com.juniormargalho.whatsapp.R;
 import com.juniormargalho.whatsapp.config.ConfiguracaoFirebase;
+import com.juniormargalho.whatsapp.fragment.ContatosFragment;
+import com.juniormargalho.whatsapp.fragment.ConversasFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add(getString(R.string.aba_conversas), ConversasFragment.class)
+                .add(getString(R.string.aba_contatos), ContatosFragment.class)
+                .create());
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+        SmartTabLayout viewPagerTab = findViewById(R.id.viewPagerTab);
+        viewPagerTab.setViewPager(viewPager);
+
     }
 
     @Override
