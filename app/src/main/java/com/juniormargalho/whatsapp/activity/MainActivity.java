b9149f0c2c07e41dcdc97bab1e118d63f0fd5d34
juniormargalho <juniormargalho@gmail.com>
2019-11-16 10:41:44 -0300

    criacaoMenu

--- app/src/main/java/com/juniormargalho/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/juniormargalho/whatsapp/activity/MainActivity.java
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.juniormargalho.whatsapp.R;
+import com.juniormargalho.whatsapp.config.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,37 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar);
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuSair :
+                deslogarUsuario();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deslogarUsuario(){
+
+        try {
+            autenticacao.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
