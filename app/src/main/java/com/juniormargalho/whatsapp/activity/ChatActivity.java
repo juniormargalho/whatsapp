71d6fbd7f813cfcb51b2bc2ff9ecab58bd99c199
juniormargalho <juniormargalho@gmail.com>
2019-11-25 17:43:29 -0300

    configuradoDestinatarioTelaChat

--- app/src/main/java/com/juniormargalho/whatsapp/activity/ChatActivity.java
+++ app/src/main/java/com/juniormargalho/whatsapp/activity/ChatActivity.java
@@ -1,7 +1,9 @@
 package com.juniormargalho.whatsapp.activity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -9,20 +11,51 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
 import com.juniormargalho.whatsapp.R;
+import com.juniormargalho.whatsapp.model.Usuario;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private TextView textViewNome;
+    private CircleImageView circleImageViewFoto;
+    private Usuario usuarioDestinatario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        //configuração toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //configurações iniciais
+        textViewNome = findViewById(R.id.textViewNomeChat);
+        circleImageViewFoto = findViewById(R.id.circleImageFotoChat);
+
+        //recuperar dados do usuario destinatario
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            usuarioDestinatario = (Usuario) bundle.getSerializable("chatContato");
+            textViewNome.setText(usuarioDestinatario.getNome());
+
+            String foto = usuarioDestinatario.getFoto();
+
+            if(foto != null){
+                Uri url = Uri.parse(usuarioDestinatario.getFoto());
+                Glide.with(ChatActivity.this).load(url).into(circleImageViewFoto);
+            }else {
+                circleImageViewFoto.setImageResource(R.drawable.padrao);
+            }
+        }
+
     }
 
 }
