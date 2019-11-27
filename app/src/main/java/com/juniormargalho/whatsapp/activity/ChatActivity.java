88e591074169d62d82f224846edfa91615e801ee
juniormargalho <juniormargalho@gmail.com>
2019-11-27 16:46:26 -0300

    listagemMensagensRemetente

--- app/src/main/java/com/juniormargalho/whatsapp/activity/ChatActivity.java
+++ app/src/main/java/com/juniormargalho/whatsapp/activity/ChatActivity.java
@@ -7,6 +7,8 @@ import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,6 +19,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.juniormargalho.whatsapp.R;
 import com.juniormargalho.whatsapp.adapter.MensagensAdapter;
@@ -42,6 +47,9 @@ public class ChatActivity extends AppCompatActivity {
     private RecyclerView recyclerMensagens;
     private MensagensAdapter adapter;
     private List<Mensagem> mensagens = new ArrayList<>();
+    private DatabaseReference database;
+    private DatabaseReference mensagensRef;
+    private ChildEventListener childEventListenerMensagens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,6 +97,9 @@ public class ChatActivity extends AppCompatActivity {
         recyclerMensagens.setLayoutManager(layoutManager);
         recyclerMensagens.setHasFixedSize(true);
         recyclerMensagens.setAdapter(adapter);
+
+        database = ConfiguracaoFirebase.getFirebaseDatabase();
+        mensagensRef = database.child("mensagens").child(idUsuarioRemetente).child(idUsuarioDestinatario);
     }
 
     public void enviarMensagem(View view){
@@ -115,4 +126,46 @@ public class ChatActivity extends AppCompatActivity {
         editMensagem.setText("");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarMensagens();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mensagensRef.removeEventListener(childEventListenerMensagens);
+    }
+
+    private void recuperarMensagens(){
+        childEventListenerMensagens = mensagensRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Mensagem mensagem = dataSnapshot.getValue(Mensagem.class);
+                mensagens.add(mensagem);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
--- app/src/main/java/com/juniormargalho/whatsapp/adapter/MensagensAdapter.java
+++ app/src/main/java/com/juniormargalho/whatsapp/adapter/MensagensAdapter.java
@@ -1,12 +1,19 @@
 package com.juniormargalho.whatsapp.adapter;
 
 import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.juniormargalho.whatsapp.R;
+import com.juniormargalho.whatsapp.helper.UsuarioFirebase;
 import com.juniormargalho.whatsapp.model.Mensagem;
 
 import java.util.List;
@@ -15,6 +22,8 @@ public class MensagensAdapter extends RecyclerView.Adapter<MensagensAdapter.MyVi
 
     private List<Mensagem> mensagens;
     private Context context;
+    private static final int TIPO_REMETENTE = 0;
+    private static final int TIPO_DESTINATARIO = 1;
 
     public MensagensAdapter(List<Mensagem> lista, Context c) {
         this.mensagens = lista;
@@ -24,22 +33,56 @@ public class MensagensAdapter extends RecyclerView.Adapter<MensagensAdapter.MyVi
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View item = null;
+
+        if(viewType == TIPO_REMETENTE){
+            item = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_mensagem_remetente, parent, false);
+        }else if (viewType == TIPO_DESTINATARIO){
+            item = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_mensagem_destinatario, parent, false);
+        }
+        return new MyViewHolder(item);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Mensagem mensagem = mensagens.get(position);
+        String msg = mensagem.getMensagem();
+        String imagem = mensagem.getImagem();
 
+        if(imagem != null){
+            Uri url = Uri.parse(imagem);
+            Glide.with(context).load(url).into(holder.imagem);
+            holder.mensagem.setVisibility(View.GONE); //oculta o texto
+        }else {
+            holder.mensagem.setText(msg);
+            holder.imagem.setVisibility(View.GONE); //oculta a imagem
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mensagens.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Mensagem mensagem = mensagens.get(position);
+        String idUsuario = UsuarioFirebase.getIdentificadorUsuario();
+
+        if(idUsuario.equals(mensagem.getIdUsuario())){
+            return TIPO_REMETENTE;
+        }
+        return TIPO_DESTINATARIO;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView mensagem;
+        ImageView imagem;
+
         public MyViewHolder(View itemView){
             super(itemView);
+            mensagem = itemView.findViewById(R.id.textMensagemTexto);
+            imagem = itemView.findViewById(R.id.imageMensagemFoto);
         }
     }
 }
